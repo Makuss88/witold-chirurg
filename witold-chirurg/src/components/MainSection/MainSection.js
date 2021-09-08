ccacdc771a6540520cc21437831398e3cb907d87
Grzegorz Maciborek <Maciborek.grzegorz@gmail.com>
2021-09-08 16:01:16 +0200

    change dir

---
+++ witold-chirurg/src/components/MainSection/MainSection.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import classes from './MainSection.module.css'
+
+const MainSection = (props) => {
+  return (
+    <div className={classes.MainSection}>cała treść </div>
+  )
+}
+
+export default MainSection;
